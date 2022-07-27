6033e6478a34220e3e43e78c155705c638d6567b
bdsconvert <bookdatasolutions@gmail.com>
2022-07-26 20:39:53 -0400

    bdsserver updates

--- bds-server/bdsserver.js
+++ bds-server/bdsserver.js
@@ -16,27 +16,9 @@ app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, './index.html'));
 });
 
-app.get('/home', (req, res) => {
-    const titles = [];
-    const stream = fs.createReadStream(`${__dirname}/users/Madhu/file2.xml.json`, {flag: 'r', encoding: 'utf-8'});
-    //stream.pipe(res);
-    let count = 0;
-    let json = '';
-    stream.on('data', (chunk) => {
-        let jss = 0;
-        let jso = 0;
-        json += chunk;
-        while (jso !== -1) {
-            jss = json.search(/\{/g);
-            jso = json.search(/\}\s*\,*\s*[\{\]]/g);
-            if (jso !== -1) {
-                titles.push(JSON.parse(json.substring(jss,jso+1)));
-                json = json.slice(jso+1);
-            }
-        }
-        console.log('Chunk ', ++count, ` - ${titles.length-1} Titles Read`);
-    });
-    stream.on('end', (chunk) => {console.log(`${titles.length} Titles Read Completed!`, `Record ${titles.length} - ${titles[titles.length-1].title}`)});
+app.get('/home', async (req, res) => {
+
+  getTitles ('Madhu', 'file2.xml.json', 'title', '1', res);
 
     //res.sendFile(path.join(__dirname, '/src/components/bds-home.js'));
 
@@ -45,3 +27,38 @@ app.get('/home', (req, res) => {
 app.listen(port, () => {
   console.log(`BDS Server listening on port ${port}`);
 });
+/////////////////////////////////////////////////////////////////////////////////////////////
+
+const getTitles = (user, file, sortby, page, res) => {
+  console.log('Running getTitles ', user, '/', file, '/', sortby, '/', page);
+  let titles = [];
+  let sortedTitles = [];
+  const stream = fs.createReadStream(`${__dirname}/users/${user}/${file}`, {flag: 'r', encoding: 'utf-8'});
+  
+  let count = 0;
+  let json = '';
+  stream.on('data', (chunk) => {
+      let jss = 0;
+      let jso = 0;
+      json += chunk;
+      while (jso !== -1) {
+          jss = json.search(/\{/g);
+          jso = json.search(/\}\s*\,*\s*[\{\]]/g);
+          if (jso !== -1) {
+              titles.push(JSON.parse(json.substring(jss,jso+1)));
+              json = json.slice(jso+1);
+          }
+      }
+      console.log('Chunk ', ++count, ` - ${titles.length-1} Titles Read`);
+      // sort chunks
+      titles.sort((a,b) => (a.title.toUpperCase() > b.title.toUpperCase()) ? 1 : -1);
+      sortedTitles = [...sortedTitles, ...titles.slice(0,10)];
+      titles = [];
+  });
+  stream.on('end', (chunk) => {
+    // sort final
+    sortedTitles.sort((a,b) => (a.title > b.title) ? 1 : -1);
+    res.send(sortedTitles.slice(0,10));
+  });
+
+}
